2c00fec9f603b8c6d1eb3bcde3452b4fe9fb7c3d
Arfaoui Ahmed <Ahmed.Arfaoui@swisscom.com>
2020-08-18 09:01:21 +0200

    adds Tensor dep and integrate startup files

--- src/index.js
+++ src/index.js
@@ -1,14 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import { Canvas, Controls } from "./App";
 import * as serviceWorker from './serviceWorker';
+import * as tf from "@tensorflow/tfjs";
+
+
+
+const model = tf.loadLayersModel("./model/model.json");
+const labels = require("./labels.json");
+let ref = React.createRef();
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <div>
+        <Canvas ref={ref} />
+        <Controls theCanvas={ref} model={model} labels={labels} />
+    </div>,
+    document.getElementById("root")
 );
 
 // If you want your app to work offline and load faster, you can change
