1adffb5427d19a7dd1293374d9e19d333ca96edb
Ahmed Arfaoui <ahmed.arfaoui@swisscom.com>
2020-10-18 13:46:06 +0200

    - replaces the br tag with slash n

--- src/components/AppText.js
+++ src/components/AppText.js
@@ -10,15 +10,15 @@ export function getAppropriateText(gamestatus, seconds, target) {
 const AppTexts =[
     {
         status: "gameStart",
-        text: "let's get started. You have <br/>secondsValue seconds to draw a roundValue <br/>in the box on the left."
+        text: "let's get started. You have \nsecondsValue seconds to draw a roundValue \nin the box on the left."
     },
     {
         status: "gameSuccess",
-        text: "You know, that was a damn <br/>fine sketch! You have <br/>secondsValue seconds to draw a roundValue."
+        text: "You know, that was a damn \nfine sketch! You have \nsecondsValue seconds to draw a roundValue."
     },
     {
         status: "gameFailure",
-        text: "Not good enough, but here's <br/>an other chance. You <br/>have SecondsValue seconds to draw <br/>a car."
+        text: "Not good enough, but here's \nan other chance. You \nhave SecondsValue seconds to draw \na car."
     },
     {
         status: "gameEndSuccess",
@@ -26,11 +26,11 @@ const AppTexts =[
     },
     {
         status: "gameEndFailure",
-        text: "Womp,womp... Try harder next time,and<br/>maybe you'll score more than just 2 points."
+        text: "Womp,womp... Try harder next time,and\nmaybe you'll score more than just 2 points."
     },
     {
         status: "welcome",
-        text: "This game has been modeled-off Google`s <a href=\"https://quickdraw.withgoogle.com/\">Quick Draw!</a> game,<br/>and uses a sampling from the Quick, Draw! <a href=\"https://github.com/googlecreativelab/quickdraw-dataset\">dataset</a>."
+        text: "This game has been modeled-off Google`s <a href=\"https://quickdraw.withgoogle.com/\">Quick Draw!</a> game,\n and uses a sampling from the Quick, Draw! <a href=\"https://github.com/googlecreativelab/quickdraw-dataset\">dataset</a>."
     },{
         status: "toYouby",
         text: "Brought to you by the EPFL extention School."
