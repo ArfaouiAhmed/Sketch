58e5e65337b32e5daf4f083c82db3561ea3bfac3
Ahmed Arfaoui <ahmed.arfaoui@swisscom.com>
2020-11-01 16:06:17 +0100

    Updates

---
+++ precache-manifest.ff6c6f52dcb5c41341f85c7649f9b5a8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0ce5a5b4a11c6301f5e076ea882d43d8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "062cbe01f15deb9a6bfd",
+    "url": "/static/css/2.87d4210f.chunk.css"
+  },
+  {
+    "revision": "a0e2fe84e0849be0092a",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "062cbe01f15deb9a6bfd",
+    "url": "/static/js/2.3ed8cd81.chunk.js"
+  },
+  {
+    "revision": "4546a110ca84ca741d37383fbcc3327b",
+    "url": "/static/js/2.3ed8cd81.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a0e2fe84e0849be0092a",
+    "url": "/static/js/main.6f2397e7.chunk.js"
+  },
+  {
+    "revision": "87c14c9b2478cfb3522d",
+    "url": "/static/js/runtime-main.e8b920fb.js"
+  }
+]);
